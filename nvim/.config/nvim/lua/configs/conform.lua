4bfa15c046ed7022e165078776a76462e3e8595d
Toran Sahu <toran.sahu@yahoo.com>
2026-05-05 15:54:16 +0530

    nvim: Configure conform for formatting and auto formatting per .ext

--- nvim/.config/nvim/lua/configs/conform.lua
+++ nvim/.config/nvim/lua/configs/conform.lua
@@ -1,15 +1,25 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    python = { "ruff_organize_imports", "ruff_format" },
   },
-
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  -- Format on save config
+  format_on_save = function(bufnr)
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+    -- Only enable for these filetypes
+    local ft = vim.bo[bufnr].filetype
+    local enable_ft = { "lua", "css", "html" } -- Add filetypes to enable
+    if vim.tbl_contains(enable_ft, ft) then
+      return { timeout_ms = 500, lsp_fallback = true }
+    end
+  end,
 }
 
 return options
